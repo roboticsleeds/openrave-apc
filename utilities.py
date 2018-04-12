2168725dafe3c00eaa6012e67d3ffbf0405417a7
Rafael Papallas <sc13rp@leeds.ac.uk>
2018-04-12 17:43:17 +0100

    Fixed the y-axis error when the shelf was moved

--- utilities.py
+++ utilities.py
@@ -6,10 +6,10 @@ import time
 import IPython
 
 class Box(object):
-    def __init__(self):
+    def __init__(self, initial_y):
        self.z = 0.81873
-       self.lower_y = -0.4202331
-       self.upper_y = -0.0103842
+       self.lower_y = initial_y + 0.4202331
+       self.upper_y = initial_y + 0.0103842
 
        self.second_col = 0.26268633
        self.third_col = 0.30568633
@@ -31,67 +31,66 @@ class Box(object):
         return transform
 
 class Box_J(Box):
-    def __init__(self, initial_x):
-        super(Box_J, self).__init__()
+    def __init__(self, initial_x, initial_y):
+        super(Box_J, self).__init__(initial_y)
         self.lower_x = initial_x + 0.01
         self.upper_x = self.lower_x + 0.25
-        # self.z = initial_z
 
 class Box_G(Box_J):
-    def __init__(self, initial_x):
-        super(Box_G, self).__init__(initial_x)
+    def __init__(self, initial_x, initial_y):
+        super(Box_G, self).__init__(initial_x, initial_y)
         self.z += self.second_row
 
 class Box_D(Box_G):
-    def __init__(self, initial_x):
-        super(Box_D, self).__init__(initial_x)
+    def __init__(self, initial_x, initial_y):
+        super(Box_D, self).__init__(initial_x, initial_y)
         self.z += self.third_row
 
 class Box_A(Box_D):
-    def __init__(self, initial_x):
-        super(Box_A, self).__init__(initial_x)
+    def __init__(self, initial_x, initial_y):
+        super(Box_A, self).__init__(initial_x, initial_y)
         self.z += self.forth_row
 
 class Box_K(Box_J):
-    def __init__(self, initial_x):
-        super(Box_K, self).__init__(initial_x)
+    def __init__(self, initial_x, initial_y):
+        super(Box_K, self).__init__(initial_x, initial_y)
         self.lower_x += self.second_col
         self.upper_x = self.lower_x + 0.25
 
 class Box_H(Box_K):
-    def __init__(self, initial_x):
-        super(Box_H, self).__init__(initial_x)
+    def __init__(self, initial_x, initial_y):
+        super(Box_H, self).__init__(initial_x, initial_y)
         self.z += self.second_row
 
 class Box_E(Box_H):
-    def __init__(self, initial_x):
-        super(Box_E, self).__init__(initial_x)
+    def __init__(self, initial_x, initial_y):
+        super(Box_E, self).__init__(initial_x, initial_y)
         self.z += self.third_row
 
 class Box_B(Box_E):
-    def __init__(self, initial_x):
-        super(Box_B, self).__init__(initial_x)
+    def __init__(self, initial_x, initial_y):
+        super(Box_B, self).__init__(initial_x, initial_y)
         self.z += self.forth_row
 
 class Box_L(Box_K):
-    def __init__(self, initial_x):
-        super(Box_L, self).__init__(initial_x)
+    def __init__(self, initial_x, initial_y):
+        super(Box_L, self).__init__(initial_x, initial_y)
         self.lower_x += self.third_col
         self.upper_x = self.lower_x + 0.25
 
 class Box_I(Box_L):
-    def __init__(self, initial_x):
-        super(Box_I, self).__init__(initial_x)
+    def __init__(self, initial_x, initial_y):
+        super(Box_I, self).__init__(initial_x, initial_y)
         self.z += self.second_row
 
 class Box_F(Box_I):
-    def __init__(self, initial_x):
-        super(Box_F, self).__init__(initial_x)
+    def __init__(self, initial_x, initial_y):
+        super(Box_F, self).__init__(initial_x, initial_y)
         self.z += self.third_row
 
 class Box_C(Box_F):
-    def __init__(self, initial_x):
-        super(Box_C, self).__init__(initial_x)
+    def __init__(self, initial_x, initial_y):
+        super(Box_C, self).__init__(initial_x, initial_y)
         self.z += self.forth_row
 
 
@@ -101,22 +100,19 @@ class Utilities:
         self.env.SetViewer('qtcoin')
         self.env.Load('amazon_picking_challenge_env.xml')
 
-        self.amazon_objects_names = [  "champion_copper_plus_spark_plug",          "cheezit_big_original",
-                                        "crayola_64_ct",                            "dove_beauty_bar",
-                                        "elmers_washable_no_run_school_glue",
-                                       #"dr_browns_bottle_brush",
-                                       "expo_dry_erase_board_eraser",              "feline_greenies_dental_treats",
-                                       "first_years_take_and_toss_straw_cups",     "genuine_joe_plastic_stir_sticks",
-                                       "highland_6539_self_stick_notes",           "kong_air_dog_squeakair_tennis_ball",
-                                       "kong_duck_dog_toy",                        "kong_sitting_frog_dog_toy",
-                                       "laugh_out_loud_joke_book",                 "kygen_squeakin_eggs_plush_puppies",
-                                       "mark_twain_huckleberry_finn",              "mead_index_cards",
-                                       "mommys_helper_outlet_plugs",               "munchkin_white_hot_duck_bath_toy",
-                                       "one_with_nature_soap_dead_sea_mud",        "oreo_mega_stuf",
-                                       "paper_mate_12_count_mirado_black_warrior", "rollodex_mesh_collection_jumbo_pencil_cup",
-                                       "safety_works_safety_glasses",              "sharpie_accent_tank_style_highlighters",
-                                       "stanley_66_052"
-                                     ]
+        self.amazon_objects_names = ["champion_copper_plus_spark_plug", "cheezit_big_original",
+                                     "crayola_64_ct", "dove_beauty_bar", "elmers_washable_no_run_school_glue",
+                                     "dr_browns_bottle_brush", "expo_dry_erase_board_eraser",
+                                     "feline_greenies_dental_treats", "first_years_take_and_toss_straw_cups",
+                                     "genuine_joe_plastic_stir_sticks", "highland_6539_self_stick_notes",
+                                     "kong_air_dog_squeakair_tennis_ball", "kong_duck_dog_toy",
+                                     "kong_sitting_frog_dog_toy", "laugh_out_loud_joke_book",
+                                     "kygen_squeakin_eggs_plush_puppies", "mark_twain_huckleberry_finn",
+                                     "mead_index_cards", "mommys_helper_outlet_plugs",
+                                     "munchkin_white_hot_duck_bath_toy", "one_with_nature_soap_dead_sea_mud",
+                                     "oreo_mega_stuf", "paper_mate_12_count_mirado_black_warrior",
+                                     "rollodex_mesh_collection_jumbo_pencil_cup", "safety_works_safety_glasses",
+                                     "sharpie_accent_tank_style_highlighters", "stanley_66_052"]
 
         self.amazon_objects = map(self.env.GetKinBody, self.amazon_objects_names)
         random.shuffle(self.amazon_objects)
@@ -126,23 +122,9 @@ class Utilities:
 
         self.shelf = self.env.GetKinBody("kiva_pod")
         initial_x = self.shelf.GetTransform()[0][3]
+        initial_y = self.shelf.GetTransform()[1][3]
 
-        self.box_a = Box_A(initial_x)
-        self.box_b = Box_B(initial_x)
-        self.box_c = Box_C(initial_x)
-        self.box_d = Box_D(initial_x)
-        self.box_e = Box_E(initial_x)
-        self.box_f = Box_F(initial_x)
-        self.box_g = Box_G(initial_x)
-        self.box_h = Box_H(initial_x)
-        self.box_i = Box_I(initial_x)
-        self.box_j = Box_J(initial_x)
-        self.box_k = Box_K(initial_x)
-        self.box_l = Box_L(initial_x)
-
-        self.boxes = [self.box_a, self.box_b, self.box_c, self.box_d, self.box_e,
-                      self.box_f, self.box_g, self.box_h, self.box_i, self.box_j,
-                      self.box_k, self.box_l]
+        self.boxes = [box(initial_x, initial_y) for box in [Box_A, Box_B, Box_C, Box_D, Box_E, Box_F, Box_G, Box_H, Box_I, Box_J, Box_K, Box_L]]
 
         object_i = 0
         box_index = 0
